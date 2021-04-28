134c078bed5c234107b62ac6817c4892921c55ae
Haozhicao <helloted@live.com>
2021-04-28 16:52:29 +0800

    update

--- run.sh
+++ run.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-curl -L https://git.code.oa.com/GameHelperTeam/commit-msg/raw/master/commit-msg -o commit-msgddd
-
-
-# mkdir -p -m 777 .git/hooks
-# mv commit-msg.py .git/hooks/
+curl -L https://raw.githubusercontent.com/helloted/commit-msg/master/commit-msg-temp -o commit-msg
+mkdir -p -m 777 .git/hooks
+mv commit-msg .git/hooks/
 
 
 
