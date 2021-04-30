180f20cc125ce236f22bf2683b9840cd76a361d3
Haozhicao <helloted@live.com>
2021-04-30 20:57:13 +0800

    feat:python3:bash

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-curl -L https://raw.githubusercontent.com/helloted/commit-msg/master/commit-msg-python -o commit-msg
+if [ $1 == "python3" ];then
+    curl -L https://raw.githubusercontent.com/helloted/commit-msg/master/commit-msg-python3 -o commit-msg
+else
+    curl -L https://raw.githubusercontent.com/helloted/commit-msg/master/commit-msg-python -o commit-msg
+fi
 mkdir -p -m 777 .git/hooks
 mv commit-msg .git/hooks/
 chmod +x  .git/hooks/commit-msg
