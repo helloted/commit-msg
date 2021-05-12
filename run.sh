e5e84311401f4e6910003119814402846ee808db
Haozhicao <helloted@live.com>
2021-05-12 19:00:02 +0800

    feat:命令:命令执行

--- run.sh
+++ run.sh
@@ -5,14 +5,23 @@ if [ ! -d ".git" ]; then
   exit 8
 fi
 
-if [[ $1 == "python3" ]];then
-    curl -L https://raw.githubusercontent.com/helloted/commit-msg/master/commit-msg-python3 -o commit-msg
-else
-    curl -L https://raw.githubusercontent.com/helloted/commit-msg/master/commit-msg-python -o commit-msg
-fi
-mkdir -p -m 777 .git/hooks
-mv commit-msg .git/hooks/
-chmod +x  .git/hooks/commit-msg
+{
+    if [[ $1 == "python2" ]];then
+        curl -L https://raw.githubusercontent.com/helloted/commit-msg/master/commit-msg-python2 -o commit-msg
+    else
+        curl -L https://raw.githubusercontent.com/helloted/commit-msg/master/commit-msg-python3 -o commit-msg
+    fi
+    mkdir -p -m 777 .git/hooks
+    mv commit-msg .git/hooks/
+    chmod +x  .git/hooks/commit-msg
+}&&{
+    echo -e "\033[32m\033[01m\033[05m commit-msg success! \033[0m"
+}
+
+
+
+
+
 
 
 
